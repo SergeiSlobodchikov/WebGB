d301b1fc3ff68699ee693730b336cc53726421e0
Sergei Slobodchikov <cerhclobodhikow@mail.ru>
2023-02-18 15:38:29 +0300

    Домашнее задание семинар 3

---
+++ Seminar3/2.js
@@ -0,0 +1,6 @@
+function greeting(name) {
+  console.log(`Привет, ${name}!`);
+}
+
+const userName = prompt("Как тебя зовут?");
+greeting(userName);
---
+++ Seminar3/main.js
@@ -0,0 +1,28 @@
+// function sumNum(a, b){
+//   return a+b;
+// }
+
+// const sumNum = (a, b) => a + b;
+
+// const num1 = +prompt('Введите первое число:');
+// const num2 = +prompt('Введите второе число:');
+
+// alert(`результат сложение чисел ${num1} и ${num2} равен ${sumNum(num1, num2)}`)
+// const userName = prompt('Как Вас зовут?')
+// function helloName(name) {
+//     console.log(`Привет, ${name}`)
+// }
+// helloName(userName)
+
+// function getMaxEvenElement(arr) {
+//   let max = arr[0];
+//   for (let i = 0; i < arr.length; i += 2) {
+//       if (arr[i] > max) {
+//           max = arr[i];
+//       }
+//   }
+//   return max;
+// }
+// console.log(getMaxEvenElement([5, 7, -1, 12, 3, 0])); // 5
+// console.log(getMaxEvenElement([4, -12, 29, 6, 31, 92, -50])); 
+https://pastebin.com/hBhyrtmD
