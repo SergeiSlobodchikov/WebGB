7cb98adacf950b5ddcd45b106f3d8726f7e821cc
Sergei Slobodchikov <cerhclobodhikow@mail.ru>
2023-02-25 20:42:01 +0300

    Домашнее задание семинар 3

--- Seminar3/1.js
+++ Seminar3/1.js
@@ -1,3 +1,3 @@
-const cels = prompt('Введите температуру в градусах Цельсия');
+const cels = +prompt('Введите температуру в градусах Цельсия');
 const farh = (cels * 1.8 + 32).toFixed(1);
 alert(`Цельсий: ${cels}, Фаренгейт: ${farh}`);
